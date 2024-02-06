4cbbf01241b3060dc543675f6edba0e1ca83cc75
gwj <76028513+ganwj@users.noreply.github.com>
2024-02-06 16:22:09 +0800

    Complete exercise 04 remove from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...toRemove) {
+    const removed = [];
+    for (const ele of arr) {
+        if (toRemove.includes(ele)) continue;
+        removed.push(ele);
+    }
 
+    return removed;
 };
 
 // Do not edit below this line
