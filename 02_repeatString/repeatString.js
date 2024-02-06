43efd3f2fc773520938852d910901fc188a633db
gwj <76028513+ganwj@users.noreply.github.com>
2024-02-06 14:48:16 +0800

    Complete exercise 02 repeat string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function (str, num) {
+    if (num < 0) return "ERROR";
 
+    let rString = "";
+    for (let i = 1; i <= num; i++) {
+        rString += str;
+    }
+
+    return rString;
 };
 
 // Do not edit below this line
