ea240026e27e9750c27ee99eb64ef4aeddc72524
gwj <76028513+ganwj@users.noreply.github.com>
2024-02-09 16:39:42 +0800

    Complete exercise 08 calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,40 @@
-const add = function() {
-	
+const add = function (a, b) {
+    return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+    return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+    return arr.reduce((total, value) => total + value, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+    return arr.reduce((product, value) => product * value, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+    return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+    let i = 1,
+        factorial = 1;
+    while (i <= num) {
+        factorial *= i;
+        i++;
+    }
+
+    return factorial;
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial,
 };
