e30527f9e3d048e15692a12a563adf7804718d4c
gwj <76028513+ganwj@users.noreply.github.com>
2024-02-09 23:15:38 +0800

    Complete exercise 12 find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    let oldest = -Infinity;
 
+    return people.reduce((obj, person) => {
+        if (!person.yearOfDeath) {
+            currentYear = new Date().getFullYear();
+            age = currentYear - person.yearOfBirth;
+        } else {
+            age = person.yearOfDeath - person.yearOfBirth;
+        }
+
+        if (age > oldest) {
+            oldest = age;
+            obj = person;
+        }
+
+        return obj;
+    }, {});
 };
 
 // Do not edit below this line
