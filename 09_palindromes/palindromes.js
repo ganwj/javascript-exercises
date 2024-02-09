e3095fab583f9d853432a1cb391903164bc84f8a
gwj <76028513+ganwj@users.noreply.github.com>
2024-02-09 17:30:11 +0800

    Complete exercise 09 palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,35 @@
-const palindromes = function () {
+function isAlphanumeric(char) {
+    return (
+        ("a".charCodeAt() <= char.charCodeAt() &&
+            char.charCodeAt() <= "z".charCodeAt()) ||
+        ("A".charCodeAt() <= char.charCodeAt() &&
+            char.charCodeAt() <= "Z".charCodeAt()) ||
+        ("0".charCodeAt() <= char.charCodeAt() &&
+            char.charCodeAt() <= "9".charCodeAt())
+    );
+}
 
+const palindromes = function (str) {
+    let i = 0,
+        j = str.length - 1;
+
+    while (i < j) {
+        if (isAlphanumeric(str[i])) {
+            if (isAlphanumeric(str[j])) {
+                if (str[i].toLowerCase() !== str[j].toLowerCase()) {
+                    return false;
+                }
+                i++;
+                j--;
+            } else {
+                j--;
+            }
+        } else {
+            i++;
+        }
+    }
+
+    return true;
 };
 
 // Do not edit below this line
