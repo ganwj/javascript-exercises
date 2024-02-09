0f2c020f818fad987c07d3b395ee27e4d3998074
gwj <76028513+ganwj@users.noreply.github.com>
2024-02-09 19:07:00 +0800

    Complete exercise 10 and 11

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,28 @@
-const fibonacci = function() {
+/* 
+    SET current value to 1
+    SET previous value to 0
+    FOR each iteration from 1 up to target index (exclusive)
+        SET temp to current value
+        ADD previous value to current value
+        SET previous value to temp
+    RETURN current value
+*/
 
+const fibonacci = function (num) {
+    let index = Number(num);
+    if (index < 0) return "OOPS";
+    if (index === 0) return 0;
+
+    let prev = 0,
+        curr = 1;
+
+    for (let i = 1; i < index; i++) {
+        let temp = curr;
+        curr += prev;
+        prev = temp;
+    }
+
+    return curr;
 };
 
 // Do not edit below this line
